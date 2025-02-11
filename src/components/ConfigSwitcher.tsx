0f43133a7069c3d8c8879af7435cdaff9184407c
Andy Truong <andyhtruong01@gmail.com>
2025-02-11 16:45:48 -0500

    bug

--- src/components/ConfigSwitcher.tsx
+++ src/components/ConfigSwitcher.tsx
@@ -250,7 +250,6 @@ export function ConfigSwitcher({
             {othersFiltered.length > 0 && (
               <Tabs.Tab value="Others">Your Studies</Tabs.Tab>
             )}
-            <Tabs.Tab value="Yours">Your Studies</Tabs.Tab>
             <Tabs.Tab value="Demos">Demo Studies</Tabs.Tab>
             <Tabs.Tab value="Examples">Example Studies</Tabs.Tab>
             <Tabs.Tab value="Tutorials">Tutorials</Tabs.Tab>
